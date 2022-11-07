8a4f30c924f9a181f417f9e8a6e93801041deac1
Oleksandr Paryshkura <pasworkit@gmail.com>
2022-11-07 12:57:34 +0200

    Resolve conflicts and some update Main Page(not finish)

--- client/src/components/SectionFeedback/SectionFeedback.js
+++ client/src/components/SectionFeedback/SectionFeedback.js
@@ -1,70 +1,76 @@
 import {
-  Avatar, Card, CardContent, CardHeader, Container, Typography,
+  Avatar, Card, CardContent, CardHeader, Typography,
 } from '@mui/material';
 import styles from './SectionFeedback.module.scss';
 
 function SectionFeedback() {
   return (
-    <Container>
-      <h2 className={styles.title}>Отзывы о Home Decor</h2>
-      <div className={styles.wrapper}>
+    <div>
+      <h2 className={styles.title}>Reviews about Home Decor</h2>
 
-        <Card sx={{ maxWidth: 345 }}>
-          <CardHeader
-            avatar={(
-              <Avatar sx={{ backgroundColor: 'red' }} aria-label="recipe">
-                R
-              </Avatar>
+      <div className={styles.wrapper}>
+        <div>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+              avatar={(
+                <Avatar sx={{ backgroundColor: 'red' }} aria-label="recipe">
+                  R
+                </Avatar>
             )}
-            title="FIO"
-            subheader="September 14, 2016"
-          />
-          <CardContent>
-            <div>
-              This impressive paella is a perfect party dish and a fun meal to cook
-              together with your guests. Add 1 cup of frozen peas along with the mussels,
-              if you like.
-            </div>
-          </CardContent>
-        </Card>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardHeader
-            avatar={(
-              <Avatar sx={{ backgroundColor: 'red' }} aria-label="recipe">
-                R
-              </Avatar>
+              title="FIO"
+              subheader="September 14, 2016"
+            />
+            <CardContent>
+              <div>
+                This impressive paella is a perfect party dish and a fun meal to cook
+                together with your guests. Add 1 cup of frozen peas along with the mussels,
+                if you like.
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <div>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+              avatar={(
+                <Avatar sx={{ backgroundColor: 'red' }} aria-label="recipe">
+                  R
+                </Avatar>
             )}
-            title="FIO"
-            subheader="September 14, 2016"
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              This impressive paella is a perfect party dish and a fun meal to cook
-              together with your guests. Add 1 cup of frozen peas along with the mussels,
-              if you like.
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardHeader
-            avatar={(
-              <Avatar sx={{ backgroundColor: 'red' }} aria-label="recipe">
-                R
-              </Avatar>
+              title="FIO"
+              subheader="September 14, 2016"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to cook
+                together with your guests. Add 1 cup of frozen peas along with the mussels,
+                if you like.
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+        <div>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+              avatar={(
+                <Avatar sx={{ backgroundColor: 'red' }} aria-label="recipe">
+                  R
+                </Avatar>
             )}
-            title="FIO"
-            subheader="September 14, 2016"
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              This impressive paella is a perfect party dish and a fun meal to cook
-              together with your guests. Add 1 cup of frozen peas along with the mussels,
-              if you like.
-            </Typography>
-          </CardContent>
-        </Card>
+              title="FIO"
+              subheader="September 14, 2016"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to cook
+                together with your guests. Add 1 cup of frozen peas along with the mussels,
+                if you like.
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
       </div>
-    </Container>
+    </div>
 
   );
 }
