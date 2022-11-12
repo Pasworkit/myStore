b84175b0447d6329818717bba6921d6ca9052a72
Volodymyr Shestopalov <svv121@gmail.com>
2022-11-12 21:06:36 +0200

    Favorites page is working

--- client/src/components/FavoritesProduct/FavoritesProduct.js
+++ client/src/components/FavoritesProduct/FavoritesProduct.js
@@ -1,38 +1,37 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import styles from './FavoritesProduct.module.scss';
 import Card from '../Card/Card';
 
 function FavoritesProduct() {
   const products = useSelector((state) => state.productsAll.products);
-  const favoriteArr = JSON.parse(localStorage.getItem('favoriteArr')) || [];
+
+  const [favoriteArr, setFavoriteArr] = useState(JSON.parse(localStorage.getItem('favoriteArr')) || []);
+  const toggleFavoriteStatus = (id) => {
+    const index = favoriteArr.indexOf(id);
+    const newFavoriteArr = [...favoriteArr];
+    if (index !== -1) {
+      newFavoriteArr.splice(index, 1);
+    } else {
+      newFavoriteArr.push(id);
+    }
+    setFavoriteArr(newFavoriteArr);
+    localStorage.setItem('favoriteArr', JSON.stringify(newFavoriteArr));
+  };
   const productsInFavorites = products.filter((el) => favoriteArr.includes(el.id));
+  const FavoritesTitle = productsInFavorites.length > 0 ? 'Your favorites' : 'You have not chosen any favorites yet';
   // eslint-disable-next-line max-len
-  const FavoritesItems = productsInFavorites.map((item) => <Card key={item.id} productCardData={item} />);
+  const FavoritesItems = productsInFavorites.map((item) => <Card toggleFavoriteStatus={toggleFavoriteStatus} key={item.id} productCardData={item} />);
 
   return (
-    <>
-      <li className={styles.favoritesProductItem}>
-        <div className={styles.favoritesProductItemWrapper}>
-          <img className={styles.favoritesImg} src="./products/img/139485.jpg" alt="img" />
-          <span className={styles.favoritesProductName}>Marble Queen pothos</span>
-          <p className={styles.favoritesProductPrice}>18.99 BYN</p>
-          <div className={styles.wrapperBtnAdd}>
-            <button className={styles.favoritesButtonAdd} type="button">Add to cart</button>
-          </div>
-          <button type="button" className={styles.favoritesDelet}>
-            <span className={styles.favoritesDeletFirstLine} />
-            <span className={styles.favoritesDeletSecondLine} />
-          </button>
-        </div>
-      </li>
-
-      <section className={styles.favoritesPageContainer}>
-        <div className={styles.favoritesItemsWrapper}>
-          {FavoritesItems}
-        </div>
-      </section>
-
-    </>
+    <section className={styles.favoritesPageContainer}>
+      <h1 className={styles.favoritesTitle}>
+        {FavoritesTitle}
+      </h1>
+      <div className={styles.favoritesItemsWrapper}>
+        {FavoritesItems}
+      </div>
+    </section>
   );
 }
 
--- client/src/store/favorites/actionCreatorsFavorites.js
+++
@@ -1,5 +0,0 @@
-// import { ADD_FAVORITES } from './actionsFavorites';
-//
-// export const addFavoritesProduct = () => async (dispatch) => {
-//
-// };
