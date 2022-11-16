94a138d99ff88cfb2024846453a7c5df5c99633f
Evgeniy Yaloza <zyaloza@gmail.com>
2022-11-16 20:10:30 +0200

    fix counter cartIcon

--- client/src/components/FavoritesProduct/FavoritesProduct.js
+++ client/src/components/FavoritesProduct/FavoritesProduct.js
@@ -32,19 +32,19 @@ function FavoritesProduct() {
       <div className={styles.favoritesItemsWrapper}>
         {FavoritesItems}
 
-    <li className={styles.favoritesProductItem}>
-      <div className={styles.favoritesProductItemWrapper}>
-        <img className={styles.favoritesImg} src="./products/img/139485.jpg" alt="img" />
-        <span className={styles.favoritesProductName}>Marble Queen pothos</span>
-        <p className={styles.favoritesProductPrice}>18.99$</p>
-        <div className={styles.wrapperBtnAdd}>
-          <button className={styles.favoritesButtonAdd} type="button">Add to cart</button>
-        </div>
-        <button type="button" className={styles.favoritesDelet}>
-          <span className={styles.favoritesDeletFirstLine} />
-          <span className={styles.favoritesDeletSecondLine} />
-        </button>
-        </div>
+        <li className={styles.favoritesProductItem}>
+          <div className={styles.favoritesProductItemWrapper}>
+            <img className={styles.favoritesImg} src="./products/img/139485.jpg" alt="img" />
+            <span className={styles.favoritesProductName}>Marble Queen pothos</span>
+            <p className={styles.favoritesProductPrice}>18.99$</p>
+            <div className={styles.wrapperBtnAdd}>
+              <button className={styles.favoritesButtonAdd} type="button">Add to cart</button>
+            </div>
+            <button type="button" className={styles.favoritesDelet}>
+              <span className={styles.favoritesDeletFirstLine} />
+              <span className={styles.favoritesDeletSecondLine} />
+            </button>
+          </div>
         </li>
       </div>
     </section>
